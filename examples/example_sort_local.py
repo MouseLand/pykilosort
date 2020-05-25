99459391fd15557384ff505317186e6606503a1a
Alexander Morley <alex@alexmorley.me>
2020-05-25 17:58:42 +0100

    Add self-downloading example to tests

---
+++ examples/example_sort_local.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+
+import numpy as np
+
+import kachery as ka
+from spikeextractors.extractors import bindatrecordingextractor as dat
+
+from pykilosort import add_default_handler, run, Bunch
+from examples.utils import create_test_directory
+
+_dir_path = os.path.dirname(os.path.realpath(__file__))
+test_dir = _dir_path+"/test/"
+dat_path = Path(test_dir + "test.bin").absolute()
+if not dat_path.is_file():
+    create_test_directory(test_dir)
+
+dir_path = dat_path.parent
+
+recording = dat.BinDatRecordingExtractor(
+        dat_path,
+        sampling_frequency=3e4,
+        numchan=4,
+        dtype='int16'
+)
+#recording.write_to_binary_dat_format(str(dat_path))
+n_channels = len(recording.get_channel_ids())
+
+
+probe = Bunch()
+probe.NchanTOT = n_channels
+probe.chanMap = np.array(range(0, n_channels))
+probe.kcoords = np.ones(n_channels)
+probe.xc = np.load(dir_path.as_posix()+'/xc.npy')
+probe.yc = np.load(dir_path.as_posix()+'/yc.npy')
+
+
+add_default_handler(level="DEBUG")
+
+params = {
+    "nfilt_factor": 8,
+    "AUCsplit": 0.85,
+    "nskip": 5 
+}
+
+def run_example():
+    run(
+        dat_path,
+        params=params,
+        probe=probe,
+        dir_path=dir_path,
+        n_channels=probe.NchanTOT,
+        dtype=np.int16,
+        sample_rate=3e4 #recording.get_sampling_frequency(),
+    )
+
+if __name__ ==  "__main__":
+    run_example()
