ace4ad73737f105df55ba6076626a8bea982d3c7
Alexander Morley <alex@alexmorley.me>
2020-05-30 14:19:40 +0100

    Let people know files are being downloaded

--- examples/utils.py
+++ examples/utils.py
@@ -9,7 +9,9 @@ def create_test_directory(test_dir_path):
     except FileExistsError:
         pass
 
+    print(f"Downloading test files from {S3_BUCKET_BASE_URL} ..."
     for file_ in ['xc.npy', 'yc.npy', 'test.bin']:
         url = S3_BUCKET_BASE_URL + file_
         filename = test_dir_path + file_
         urllib.request.urlretrieve(url, filename)
+    print("Done")
