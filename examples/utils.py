f500fbe83e8db2cc668a5e945a7e9ff3dc0db21b
Alexander Morley <alex@alexmorley.me>
2020-05-30 14:22:07 +0100

    Hotfix

--- examples/utils.py
+++ examples/utils.py
@@ -9,7 +9,7 @@ def create_test_directory(test_dir_path):
     except FileExistsError:
         pass
 
-    print(f"Downloading test files from {S3_BUCKET_BASE_URL} ..."
+    print(f"Downloading test files from {S3_BUCKET_BASE_URL} ...")
     for file_ in ['xc.npy', 'yc.npy', 'test.bin']:
         url = S3_BUCKET_BASE_URL + file_
         filename = test_dir_path + file_
