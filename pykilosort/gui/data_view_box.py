92d38cd8c01ef8321e1d91d11ac4c7fd43935efe
shashwatsridhar <shashwat.sridhar.95@gmail.com>
2020-06-18 17:30:49 +0200

    Refactoring of UI code

--- pykilosort/gui/data_view_box.py
+++ pykilosort/gui/data_view_box.py
@@ -6,67 +6,76 @@ class DataViewBox(QtWidgets.QGroupBox):
 
     def __init__(self, parent):
         QtWidgets.QGroupBox.__init__(self, parent=parent)
-        self.setTitle("Data View")
 
-        self.layout = QtWidgets.QVBoxLayout()
-
-        self.controls_button_layout = QtWidgets.QHBoxLayout()
         self.controls_button = QtWidgets.QPushButton("Controls")
-        self.controls_button.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        self.controls_button_layout.addWidget(self.controls_button)
 
-        self.data_view_layout = QtWidgets.QHBoxLayout()
         self.data_view_widget = pg.PlotWidget()
-        self.data_view_layout.addWidget(self.data_view_widget)
-
-        self.data_controls_layout = QtWidgets.QHBoxLayout()
+        self.data_seek_widget = pg.PlotWidget()
 
         self.traces_view_button = QtWidgets.QPushButton("Traces")
-        self.traces_view_button.setCheckable(True)
-
         self.colormap_view_button = QtWidgets.QPushButton("Colormap")
+        self.raw_button = QtWidgets.QPushButton("Raw")
+        self.whitened_button = QtWidgets.QPushButton("Whitened")
+        self.prediction_button = QtWidgets.QPushButton("Prediction")
+        self.residual_button = QtWidgets.QPushButton("Residual")
+
+        self.mode_buttons = [self.raw_button, self.whitened_button, self.prediction_button, self.residual_button]
+        self.view_buttons = [self.traces_view_button, self.colormap_view_button]
+
+        self.setup()
+
+    def setup(self):
+        self.setTitle("Data View")
+
+        layout = QtWidgets.QVBoxLayout()
+
+        controls_button_layout = QtWidgets.QHBoxLayout()
+        self.controls_button.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        controls_button_layout.addWidget(self.controls_button)
+
+        data_view_layout = QtWidgets.QHBoxLayout()
+        data_view_layout.addWidget(self.data_view_widget)
+
+        data_controls_layout = QtWidgets.QHBoxLayout()
+
+        self.traces_view_button.setCheckable(True)
         self.colormap_view_button.setCheckable(True)
         self.colormap_view_button.setChecked(True)
 
-        self.raw_button = QtWidgets.QPushButton("Raw")
         self.raw_button.setCheckable(True)
         self.raw_button.setStyleSheet("QPushButton {background-color: black; color: white;}")
         self.raw_button.toggled.connect(self.on_raw_button_toggled)
         self.raw_button.setChecked(True)
 
-        self.whitened_button = QtWidgets.QPushButton("Whitened")
         self.whitened_button.setCheckable(True)
         self.whitened_button.setStyleSheet("QPushButton {background-color: black; color: white;}")
         self.whitened_button.toggled.connect(self.on_whitened_button_toggled)
 
-        self.prediction_button = QtWidgets.QPushButton("Prediction")
         self.prediction_button.setCheckable(True)
         self.prediction_button.setStyleSheet("QPushButton {background-color: black; color: white;}")
         self.prediction_button.toggled.connect(self.on_prediction_button_toggled)
 
-        self.residual_button = QtWidgets.QPushButton("Residual")
         self.residual_button.setCheckable(True)
         self.residual_button.setStyleSheet("QPushButton {background-color: black; color: white;}")
         self.residual_button.toggled.connect(self.on_residual_button_toggled)
 
-        self.data_controls_layout.addWidget(self.traces_view_button)
-        self.data_controls_layout.addWidget(self.colormap_view_button)
-        self.data_controls_layout.addStretch(1)
-        self.data_controls_layout.addWidget(self.raw_button)
-        self.data_controls_layout.addWidget(self.whitened_button)
-        self.data_controls_layout.addWidget(self.prediction_button)
-        self.data_controls_layout.addWidget(self.residual_button)
+        data_controls_layout.addWidget(self.traces_view_button)
+        data_controls_layout.addWidget(self.colormap_view_button)
+        data_controls_layout.addStretch(1)
+        data_controls_layout.addWidget(self.raw_button)
+        data_controls_layout.addWidget(self.whitened_button)
+        data_controls_layout.addWidget(self.prediction_button)
+        data_controls_layout.addWidget(self.residual_button)
 
-        self.data_seek_layout = QtWidgets.QHBoxLayout()
-        self.data_seek_widget = pg.PlotWidget()
-        self.data_seek_layout.addWidget(self.data_seek_widget)
+        data_seek_layout = QtWidgets.QHBoxLayout()
+        data_seek_layout.addWidget(self.data_seek_widget)
 
-        self.layout.addLayout(self.controls_button_layout, 2)
-        self.layout.addLayout(self.data_view_layout, 80)
-        self.layout.addLayout(self.data_controls_layout, 3)
-        self.layout.addLayout(self.data_seek_layout, 15)
+        layout.addLayout(self.controls_button_layout, 2)
+        layout.addLayout(self.data_view_layout, 80)
+        layout.addLayout(self.data_controls_layout, 3)
+        layout.addLayout(self.data_seek_layout, 15)
 
-        self.setLayout(self.layout)
+        self.setLayout(layout)
 
     def on_raw_button_toggled(self, state):
         if state:
