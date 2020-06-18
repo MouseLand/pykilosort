622d4d6e1f37d2df72f78f88974135479cc63d98
shashwatsridhar <shashwat.sridhar.95@gmail.com>
2020-06-18 15:01:41 +0200

    Added buttons to the data view

--- pykilosort/gui/data_view_box.py
+++ pykilosort/gui/data_view_box.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 import pyqtgraph as pg
 
 
@@ -19,12 +19,37 @@ class DataViewBox(QtWidgets.QGroupBox):
         self.data_view_widget = pg.PlotWidget()
         self.data_view_layout.addWidget(self.data_view_widget)
 
+        self.data_controls_layout = QtWidgets.QHBoxLayout()
+        self.traces_view_button = QtWidgets.QPushButton("Traces")
+        self.traces_view_button.setCheckable(True)
+        self.colormap_view_button = QtWidgets.QPushButton("Colormap")
+        self.colormap_view_button.setCheckable(True)
+        self.colormap_view_button.setChecked(True)
+        self.raw_button = QtWidgets.QPushButton("Raw")
+        self.raw_button.setCheckable(True)
+        self.raw_button.setChecked(True)
+        self.whitened_button = QtWidgets.QPushButton("Whitened")
+        self.whitened_button.setCheckable(True)
+        self.prediction_button = QtWidgets.QPushButton("Prediction")
+        self.prediction_button.setCheckable(True)
+        self.residual_button = QtWidgets.QPushButton("Residual")
+        self.residual_button.setCheckable(True)
+
+        self.data_controls_layout.addWidget(self.traces_view_button)
+        self.data_controls_layout.addWidget(self.colormap_view_button)
+        self.data_controls_layout.addStretch(1)
+        self.data_controls_layout.addWidget(self.raw_button)
+        self.data_controls_layout.addWidget(self.whitened_button)
+        self.data_controls_layout.addWidget(self.prediction_button)
+        self.data_controls_layout.addWidget(self.residual_button)
+
         self.data_seek_layout = QtWidgets.QHBoxLayout()
         self.data_seek_widget = pg.PlotWidget()
         self.data_seek_layout.addWidget(self.data_seek_widget)
 
-        self.layout.addLayout(self.controls_button_layout, 3)
-        self.layout.addLayout(self.data_view_layout, 82)
+        self.layout.addLayout(self.controls_button_layout, 2)
+        self.layout.addLayout(self.data_view_layout, 80)
+        self.layout.addLayout(self.data_controls_layout, 3)
         self.layout.addLayout(self.data_seek_layout, 15)
 
         self.setLayout(self.layout)
