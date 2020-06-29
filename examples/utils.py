b34dbf1a92606e050fbb7c23305b066c8b6426f5
Alexander Morley <alex@alexmorley.me>
2020-06-29 15:22:00 +0100

    update

--- examples/utils.py
+++ examples/utils.py
@@ -3,6 +3,13 @@ import urllib.request
 
 S3_BUCKET_BASE_URL = 'https://static.alexmorley.me/pykilosort/test-recording/'
 
+
+def urlretrieve(url, filename):
+    opener = urllib.request.URLopener()
+    # Spoof a browser's user agent header because nginx doesn't like urllib
+    opener.addheader('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0')
+    filename, headers = opener.retrieve(url, filename)
+
 def create_test_directory(test_dir_path):
     try:
         os.makedirs(test_dir_path)
@@ -13,6 +20,5 @@ def create_test_directory(test_dir_path):
     for file_ in ['xc.npy', 'yc.npy', 'test.bin']:
         url = S3_BUCKET_BASE_URL + file_
         filename = test_dir_path + file_
-        print(url)
-        urllib.request.urlretrieve(url, filename)
+        urlretrieve(url, filename)
     print("Done")
