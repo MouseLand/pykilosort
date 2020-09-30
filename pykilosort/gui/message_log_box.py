c4f0358525db654d7cecca4e2c83ad17e75137a9
shashwatsridhar <shashwat.sridhar.95@gmail.com>
2020-09-30 14:59:52 +0530

    WIP: logging to message log box implemented

--- pykilosort/gui/message_log_box.py
+++ pykilosort/gui/message_log_box.py
@@ -1,8 +1,5 @@
-import sys
-import logging
+from pykilosort.gui.logger import XStream
 from PyQt5 import QtWidgets, QtCore, QtGui
-
-logger = logging.getLogger(__name__)
 
 
 class MessageLogBox(QtWidgets.QGroupBox):
@@ -15,10 +12,15 @@ class MessageLogBox(QtWidgets.QGroupBox):
         self.log_box = QtWidgets.QPlainTextEdit()
         self.log_box.setReadOnly(True)
         self.log_box.setLineWrapMode(QtWidgets.QPlainTextEdit.NoWrap)
+        self.log_box.setFont(QtGui.QFont("Monospace"))
         self.layout.addWidget(self.log_box)
+
+        XStream.stdout().messageWritten.connect(self.update_text)
+        XStream.stderr().messageWritten.connect(self.update_text)
 
         self.setLayout(self.layout)
 
+    @QtCore.pyqtSlot(str)
     def update_text(self, text):
         self.log_box.moveCursor(QtGui.QTextCursor.End)
         self.log_box.appendPlainText(text)
