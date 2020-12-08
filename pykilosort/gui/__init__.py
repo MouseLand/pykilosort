a6ba5d472fec6b11c8596f2e423613e716b4ae51
Shashwat S <shashwat.sridhar.95@gmail.com>
2020-12-07 19:11:58 -0500

    reorder imports to make GUI work again

--- pykilosort/gui/__init__.py
+++ pykilosort/gui/__init__.py
@@ -4,8 +4,6 @@ This module contains all files required by the pykilosort GUI.
 from . import probes
 from .data_view_box import DataViewBox
 from .header_box import HeaderBox
-from .launch import launcher
-from .main import KiloSortGUI
 from .message_log_box import MessageLogBox
 from .minor_gui_elements import ProbeBuilder
 from .palettes import COLORMAP_COLORS, DarkPalette
@@ -13,3 +11,6 @@ from .probe_view_box import ProbeViewBox
 from .run_box import RunBox
 from .settings_box import SettingsBox
 from .sorter import KiloSortWorker, filter_and_whiten, find_good_channels
+
+from .main import KiloSortGUI
+from .launch import launcher
