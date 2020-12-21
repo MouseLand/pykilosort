(fix update) Speed up calculation of predicted traces (#53)
Fix errors left unaddressed in PR #48 in function get_predicted_traces:

* account for cupy / numpy arrays as input
* use @ for matrix multiplication (more reliable)
* change cupy to numpy on one line

pykilosort/gui/sorter.py
@@ -71,13 +71,14 @@ def get_whitened_traces(raw_data, probe, params, whitening_matrix):
 
 
 def get_predicted_traces(matrix_U, matrix_W, sorting_result, time_limits):
-    W = np.asarray(matrix_W, dtype=np.float32)
-    U = np.asarray(matrix_U, dtype=np.float32)
+    W = cp.asnumpy(matrix_W)
+    U = cp.asnumpy(matrix_U)
 
     buffer = W.shape[0]
 
     predicted_traces = np.zeros(
-        (U.shape[0], 4 * buffer + (time_limits[1] - time_limits[0])), dtype=np.int16
+        (U.shape[0], 4 * buffer + (time_limits[1] - time_limits[0])),
+        dtype=np.int16
     )
 
     sorting_result = np.asarray(sorting_result)
@@ -87,7 +88,7 @@ def get_predicted_traces(matrix_U, matrix_W, sorting_result, time_limits):
         (time_limits[0] - buffer // 2 < all_spike_times)
         & (all_spike_times < time_limits[1] + buffer // 2)
     ).nonzero()[0]
-    
+
     spike_times = all_spike_times[included_spike_pos].astype(np.int32)
     spike_templates = sorting_result[included_spike_pos, 1].astype(np.int32)
     spike_amplitudes = sorting_result[included_spike_pos, 2]
@@ -97,14 +98,12 @@ def get_predicted_traces(matrix_U, matrix_W, sorting_result, time_limits):
         U_i = U[:, spike_templates[s], :]
         W_i = W[:, spike_templates[s], :]
 
-        addendum = np.ascontiguousarray(
-            cp.matmul(U_i, W_i.T) * amplitude, dtype=np.int16
-        )
+        addendum = (U_i @ W_i.T * amplitude).astype(np.int16)
 
         pred_pos = np.arange(buffer) + spike - time_limits[0] + buffer + buffer // 2
         predicted_traces[:, pred_pos] += addendum
 
-    output = predicted_traces[:, buffer * 2 : -buffer * 2]
+    output = predicted_traces[:, buffer * 2: -buffer * 2]
 
     return output.T
 