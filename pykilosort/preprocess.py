64e3cc6fdc1fc524e41818bd293f5ea1bf20426e
Shashwat S <shashwat.sridhar.95@gmail.com>
2020-12-22 10:22:44 -0500

    remove tqdm output for on-the-fly whitening

--- pykilosort/preprocess.py
+++ pykilosort/preprocess.py
@@ -257,7 +257,7 @@ def get_approx_whitening_matrix(raw_data, params, probe):
     # Nchan is obtained after the bad channels have been removed
     CC = cp.zeros((Nchan, Nchan))
 
-    for ibatch in tqdm(batches, desc="Computing the approx. whitening matrix"):
+    for ibatch in batches:
         i = max(0, (NT - ntbuff) * ibatch - 2 * ntbuff)
         # WARNING: we no longer use Fortran order, so raw_data is nsamples x NchanTOT
         buff = raw_data[i:i + NT - ntbuff]
