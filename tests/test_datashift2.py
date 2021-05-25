a680aa8e9e0693cdbc18c3b219e10582c2a9aea9
Kush <kushbanga@gmail.com>
2021-05-25 21:40:07 +0100

    test coverage for align_block2

--- tests/test_datashift2.py
+++ tests/test_datashift2.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from pykilosort.datashift2 import shift_data
+from pykilosort.datashift2 import shift_data, align_block2
 
 def test_shift_data():
     data = np.asfortranarray(np.random.randint(-100, 100, size=(65000, 384), dtype=np.int16))
@@ -10,3 +10,14 @@ def test_shift_data():
     assert test_result.dtype == np.int16
     assert np.isfortran(test_result)
     assert np.allclose(test_result, np.asfortranarray((data @ transformation.T).astype("int16")))
+
+def test_align_block2():
+
+    # Rigid Case
+    spike_histograms = np.zeros((20, 2, 3))
+    spike_histograms[2:4,:,0] = 1
+    spike_histograms[3:5,:,1] = 1
+    spike_histograms[4:6,:,2] = 1
+    imin, yblk, F0 = align_block2(spike_histograms, np.arange(20), 1)
+    imin -= np.mean(imin)
+    assert np.allclose(imin, np.array([[1],[0],[-1]]))
